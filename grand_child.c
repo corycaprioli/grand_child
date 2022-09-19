4ae3425c77b202afd7e135a4eee2f8f0c9a492ec
corycaprioli <cory.caprioli@csu.fullerton.edu>
2022-09-18 21:43:55 -0700

    turnin

--- grand_child.c
+++ grand_child.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
+int main(int argc, char** argv)
+{
+    pid_t child_pid;
+    child_pid = fork();
+    if (child_pid == 0)
+    {
+        pid_t grand_child_pid;
+        grand_child_pid = fork();
+        printf("This is process %d\n", getpid());
+        
+        if(grand_child_pid == 0) {
+          execlp("/usr/bin/firefox", "firefox", NULL);
+      }
+    }
+    printf("This is process %d\n", getpid());
+    wait(NULL);
+    return 0;
+}
